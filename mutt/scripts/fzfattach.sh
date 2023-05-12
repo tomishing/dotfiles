353c9e346e030fade13ca24eb2345703c87ed164
brooky <tomishing@gmail.com>
2023-05-12 12:08:32 +0900

    fzf script

--- mutt/scripts/fzfattach.sh
+++ mutt/scripts/fzfattach.sh
@@ -1,10 +1,12 @@
-#!/bin/bash
+# #!/bin/bash
+#!/bin/zsh
+
 # cd to the root folder of attachments
 # https://github.com/neomutt/neomutt/issues/1669
 cd $HOME
 
 # custom fd (fdfind on debian) for fzf, including only some extentions that I use as attachments
-export FZF_DEFAULT_COMMAND='fdfind -t f -e pdf -e png -e jpg -e zip -e tar -e gz -e rar -e html -e md --absolute-path'
+#export FZF_DEFAULT_COMMAND='fdfind -t f -e pdf -e png -e jpg -e zip -e tar -e gz -e rar -e html -e md --absolute-path'
 
 #attachment='fzf -m --prompt='Choose one/multiple file(s) to attach >' | \
 #while IFS=$'\n' read -r attachment; do
